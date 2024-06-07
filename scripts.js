58fc498dec185e73b09a211ddf81f68440e39744
raphadevflow <raphaelgomescontato7@gmail.com>
2024-06-07 11:10:34 -0300

    Conversor de moeda desafio DevClub JavaScript

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const convertButton = document.querySelector(".convert-button") 
+
+function convertValues(){
+    const  inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //Valor em Real
+    const currencyValueConverted = document.querySelector(".currency-value") // Outras moedas
+
+    const dolarToday = 5.2
+
+    const convertedValue = inputCurrencyValue / dolarToday
+
+    currencyValueToConvert.innerHTML = inputCurrencyValue 
+
+    console.log(convertedValue)
+}
+
+convertButton.addEventListener("click", convertValues)
+
