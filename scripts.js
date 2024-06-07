32f652528879cb774a033e52b411268e7fa42927
raphadevflow <raphaelgomescontato7@gmail.com>
2024-06-07 19:13:38 -0300

    alterações no html e JS entre valores e a seleção de moeda

--- scripts.js
+++ scripts.js
@@ -4,20 +4,36 @@ function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //Valor em Real
     const currencyValueConverted = document.querySelector(".currency-value") // Outras moedas
+    const currencySelect = document.querySelector(".currency-select")
+
+    console.log(currencySelect.value)
 
     const dolarToday = 5.2
+    const euroToday = 6.2
 
-    const convertedValue = inputCurrencyValue / dolarToday
+
+
+    if (currencySelect.value == "dolar") { //se o select estiver selecionado o valor de dolar, entre aqui
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+
+    if (currencySelect.value == "euro") { //se o select estiver selecionado o valor de euro, entre aqui
+        currencyValueConverted.innerHTML = Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency:"EUR"
+        }).format(inputCurrencyValue / euroToday)
+
+    }
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
     }).format(inputCurrencyValue)
-    
-    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-    }).format(convertedValue)
+
+
 
     console.log(convertedValue)
 }
